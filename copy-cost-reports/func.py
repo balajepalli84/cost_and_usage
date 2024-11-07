ebd5910b52d577b576b890c1e4d8974fda3e97f7
balajepalli84 <165810247+balajepalli84@users.noreply.github.com>
2024-11-07 09:36:59 -0600

    Add files via upload

--- copy-cost-reports/func.py
+++ copy-cost-reports/func.py
@@ -8,17 +8,15 @@ from fdk import response
 
 def handler(ctx, data: io.BytesIO = None):
     try:
-        # Do not modify these values
         reporting_namespace = 'bling'
         yesterday = datetime.now() - timedelta(days=1)
         prefix_file = f"FOCUS Reports/{yesterday.year}/{yesterday.strftime('%m')}/{yesterday.strftime('%d')}"
         print(f"prefix is {prefix_file}")
-        destination_path = '/tmp'        
+        destination_path = '/tmp'
         
-        dest_namespace='ociateam'
-        upload_bucket_name = 'cost_and_usage_reports'  # Replace with your target bucket for uploads
-        reporting_bucket = 'ocid1.tenancy.oc1..aaaaaaaaa3qmjxr43tjexx75r6gwk6vjw22ermohbw2vbxyhczksgjir7xdq'        
-
+        dest_namespace = 'ociateam'
+        upload_bucket_name = 'cost_and_usage_reports'
+        reporting_bucket = 'ocid1.tenancy.oc1..aaaaaaaaa3qmjxr43tjexx75r6gwk6vjw22ermohbw2vbxyhczksgjir7xdq'
         
         Signer = oci.auth.signers.get_resource_principals_signer() 
         object_storage = oci.object_storage.ObjectStorageClient(config={}, signer=Signer)
@@ -27,20 +25,28 @@ def handler(ctx, data: io.BytesIO = None):
         for o in report_bucket_objects.data.objects:
             object_details = object_storage.get_object(reporting_namespace, reporting_bucket, o.name)
             filename = o.name.rsplit('/', 1)[-1]
-            local_file_path = destination_path+'/'+filename            
+            local_file_path = destination_path + '/' + filename
+            
             with open(local_file_path, 'wb') as f:
                 for chunk in object_details.data.raw.stream(1024 * 1024, decode_content=False):
                     f.write(chunk)
+            
+            # Preserve the prefix structure in the destination bucket
+            destination_object_name = f"{prefix_file}/{filename}"
+            
             with open(local_file_path, 'rb') as file_content:
                 object_storage.put_object(
                     namespace_name=dest_namespace,
                     bucket_name=upload_bucket_name,
-                    object_name=filename,
+                    object_name=destination_object_name,
                     put_object_body=file_content
                 )
+                
     except (Exception, ValueError) as ex:
         logging.getLogger().info('error parsing payload: ' + str(ex))
+    
     return response.Response(
         ctx, response_data=json.dumps(
-            {"message": "Processed Files sucessfully"})
+            {"message": "Processed Files successfully"}
+        )
     )
