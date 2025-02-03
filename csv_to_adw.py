286520da6c61d8a19574f2700f37aeee108c950d
balajepalli84 <165810247+balajepalli84@users.noreply.github.com>
2025-02-03 04:59:39 -0600

    Add files via upload

---
+++ csv_to_adw.py
@@ -0,0 +1,23 @@
+import oci
+import oracledb
+
+# Connect to OCI
+config = oci.config.from_file()
+identity_client = oci.identity.IdentityClient(config)
+
+dsn ='''(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=gwtizid6xsmfvaf_u6vfaxcq64nyfskm_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))'''
+user = "ADMIN"
+password = ""
+
+# Connect to ADB
+connection = oracledb.connect(user=user, password=password, dsn=dsn)
+
+# Execute a simple query
+with connection.cursor() as cursor:
+    cursor.execute("SELECT 1 FROM DUAL")
+    result = cursor.fetchone()
+
+print(result)
+
+# Close the connection
+connection.close()
